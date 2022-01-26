a9c116057204e8b1b3bca62e4c06df581d8d5022
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-01-26 20:47:25 -0300

    Finalizada estilização da home e adicionadas media queries

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,13 +9,15 @@ export default function Home(){
                 <a className="btn btn-verde btn-assistirfinal">Assista a final</a>
             </section>
             <section className="heroi-penta container">
-                <div className="foto-heroipenta">
-                    <img src={fotoRonaldo} alt="Ronaldo Fenômeno"/>
-                </div>
-                <div className="txt-heroipenta">
-                    <h1 class="titulo">Ronaldo Fenômeno</h1>
-                    <h2 class="subtitulo">O herói do penta</h2>
-                    <p>Na etapa final, Ronaldo apareceu e garantiu o pentacampeonato para a Seleção Brasileira. Com 22 minutos, o Fenômeno pegou o rebote de Kahn, após o chute de Rivaldo, e abriu o placar. Aos 34 minutos, Kleberson apareceu mais uma vez pela direita, cruzou rasteiro, e Rivaldo fez um lindo corta-luz. Ronaldo finalizou da entrada da área, no cantinho de Oliver Kahn, e deu números finais à partida do pentacampeonato da Seleção.</p>
+                <div className="container-artigo-heroipenta">
+                    <div className="foto-heroipenta">
+                        <img src={fotoRonaldo} alt="Ronaldo Fenômeno"/>
+                    </div>
+                    <div className="txt-heroipenta">
+                        <h1 class="titulo">Ronaldo Fenômeno</h1>
+                        <h2 class="subtitulo">O herói do penta</h2>
+                        <p>Na etapa final, Ronaldo apareceu e garantiu o pentacampeonato para a Seleção Brasileira. Com 22 minutos, o Fenômeno pegou o rebote de Kahn, após o chute de Rivaldo, e abriu o placar. Aos 34 minutos, Kleberson apareceu mais uma vez pela direita, cruzou rasteiro, e Rivaldo fez um lindo corta-luz. Ronaldo finalizou da entrada da área, no cantinho de Oliver Kahn, e deu números finais à partida do pentacampeonato da Seleção.</p>
+                    </div>
                 </div>
                 <div className="numeros-heroipenta">
                         <div className="partidas-jogadas">
