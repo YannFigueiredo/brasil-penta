93a6f2ef453154727f24989b24f9f3d26d8afc94
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-01-27 21:24:08 -0300

    Iniciada página da equipe titular na copa 2002

--- src/pages/Equipe/index.js
+++ src/pages/Equipe/index.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import '../../styles/equipe.css';
+
 export default function Equipe(){
+    const [jogadores, setJogadores] = useState([
+        {id: 1, nome: 'Marcos', camisa: '01', posicao: 'goleiro', foto: '../../assets/img/jogadores/1-marcos.jpg'}
+    ]);
+    
     return(
-        <section>
-            
+        <section className="equipe-titular container">
+            <h1 className="titulo">Equipe titular</h1>
+            {
+                jogadores.map(jogador => (
+                    <article key={jogador.id}>
+                        <div className="foto-jogador">
+                            <img src={jogador.foto}/>
+                        </div>
+                        <div className="identificacao-jogador">
+                            <h3>{jogador.camisa} - {jogador.nome}</h3>
+                            <p>{jogador.posicao}</p>
+                        </div>
+                    </article>
+                ))
+            }
         </section>
     );
 }
