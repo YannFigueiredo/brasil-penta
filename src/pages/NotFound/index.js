3d360dab301fdd6ea391387fb3cf4bc3a3e661c4
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-04-12 09:47:33 -0300

    Corrigida página de NotFound

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,7 @@
 export default function NotFound(){
     return(
-        <div className="pagina-inexistente" style={{minHeight: '53vh'}}>
-            <h1 className="titulo" style={{textAlign: 'center'}}>Página não encontrada!</h1>
+        <div className="pagina-inexistente" style={{minHeight: '62vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <h1 className="titulo">Página não encontrada!</h1>
         </div>
     );
 }
