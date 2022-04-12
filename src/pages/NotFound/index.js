d127e53ac4fab15cdd13186a8c40c973bdfe71dd
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-04-12 16:41:25 -0300

    Corrigido problema de scroll da página de partidas

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,6 +1,6 @@
 export default function NotFound(){
     return(
-        <div className="pagina-inexistente" style={{minHeight: '62vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <div className="pagina-inexistente" style={{minHeight: '70vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <h1 className="titulo">Página não encontrada!</h1>
         </div>
     );
